176c684dc72943e09340a9995b9869350f7acc20
bahattinyunuscetin <bahattinyunuscetin@hotmail.com>
2025-06-08 20:51:35 +0300

    up

---
+++ nesne-yonelimli-giris/oop.cpp
@@ -0,0 +1,13 @@
+﻿class Matematik {
+	public:
+	int a;
+	int b;
+	int result=0;
+	void topla(){
+		this->result = this ->a + this -> b;
+   }
+   void print(){
+	std:: cout << this->result;
+
+   }
+};
