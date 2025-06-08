67b7e55018625fd4caa4494017126cbf2afbbc03
bahattinyunuscetin <bahattinyunuscetin@hotmail.com>
2025-06-08 20:58:44 +0300

    up

--- nesne-yonelimli-giris/oop.cpp
+++ nesne-yonelimli-giris/oop.cpp
@@ -1,13 +1,25 @@
-﻿class Matematik {
+﻿
+#include <iostream>  
+#include <string>
+
+class Matematik {
 	public:
 	int a;
 	int b;
 	int result=0;
 	void topla(){
-		this->result = this ->a + this -> b;
+		this->result = this ->a + this -> b;   // this sınıfın kendi üyerine erişim sağlamak için kullanılır
    }
    void print(){
 	std:: cout << this->result;
 
    }
 };
+
+int main(){
+	Matematik mat;  // nesne tanımlama
+	mat.a=10;
+	mat.b=10;
+	mat.topla();
+	mat.print();
+}
